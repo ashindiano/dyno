3b31a36bcb75be6a95166870e908795d67b41277
ashindiano <ashindiano@gmail.com>
2020-06-26 11:26:20 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,54 +1,48 @@
 #!/bin/bash
 #version 1.0
+declare -A commands
+commands=(
+    [open]="Opens current folder"
+    [script]="Open the 'dyno' Script file"
+    [source]="Source the Current file in Shell"
+    [help]="List all the commands the available"
+)
 
-function shaw(){
- case $1 in
-        "apis")
-            echo "Opening POSTMAN link for GoApi documents" 
-            open ""
-        ;;
-         "deployment")
-            cd /
-            echo "Opening Jenkins"
-            open ""
-        ;;
-        "repo")
-            cd /
-            echo "Opening PipIndex in Github"
-            open ""
-        ;;
-        "tickets")
-            echo "Opening JIRA"        
-            open ""
-        ;;
-        "logs")  
-            echo "Opening Loggly"        
-            open https://logshawacademy.loggly.com
+#The following code helps in auto completion
+allCommands=""
+for key in ${!commands[@]}; do
+    allCommands+="${key} "
+done
+
+complete -W "${allCommands}" dyno
+#########
+
+function dyno(){
+    case $1 in
+        
+        "open")
+            cd ~
+            echo "Opening Current Folder"
+            nautilus .
         ;;
         "script")
-            cd /
+            cd ~
             echo "Opening $BASH_SOURCE"
-            open "$BASH_SOURCE"
+            code "$BASH_SOURCE"
         ;;
         "source")
-            cd /
+            cd ~
             echo "Sourcing $BASH_SOURCE"
             source "$BASH_SOURCE"
         ;;
-        "hr")
-            echo "Opening GreyHR"        
-            open ""
-        ;;
+        
         "help"|"h"|"--help"|"-h")
-            echo "Commands available"
-            echo "apis"
-            echo "logs"
-            echo "jenkins"
-            echo "repo"
-            echo "script"
-            echo "source"
-            echo "tickets"
+            for key in ${!commands[@]}; do
+                echo ${key} - ${commands[${key}]}
+            done
         ;;
         
-  esac
+    esac
 }
+
+alias bye="systemctl poweroff"
