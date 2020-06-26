d87d83fd3f3e2b137f2d1402b7b8c68b9054f731
ashindiano <ashindiano@gmail.com>
2020-06-26 13:06:06 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -5,7 +5,9 @@ commands=(
     [open]="Opens current folder"
     [script]="Open the 'dyno' Script file"
     [source]="Source the Current file in Shell"
+    [add]="Add another source script"
     [help]="List all the commands the available"
+    [reset]="Reset the script to the factory defaults"
 )
 
 #The following code helps in auto completion
@@ -30,12 +32,33 @@ function dyno(){
             echo "Opening $BASH_SOURCE"
             code "$BASH_SOURCE"
         ;;
+        "add")
+            read -e -p "Enter the path of the new script to source: " FILE
+            fullPath=$(realpath -m $FILE | sed 's/\~\///g')
+            if test -f "$fullPath"; then
+                if  [[ "$fullPath" -ef "$BASH_SOURCE" ]]; then
+                    echo "You cannot add Me Again"
+                else
+                    echo "source $fullPath"  >> "$BASH_SOURCE"
+                    source "$BASH_SOURCE"
+                fi
+            else
+                echo "$(realpath -m $fullPath) File does not exist."
+            fi
+        ;;
         "source")
             cd ~
             echo "Sourcing $BASH_SOURCE"
+            
             source "$BASH_SOURCE"
         ;;
-        
+        "reset")
+            echo "Are you sure you wanna reset Dyno? Yes/No"
+            read answer
+            # if $answer = "Yes"
+            
+            # fi
+        ;;
         "help"|"h"|"--help"|"-h")
             for key in ${!commands[@]}; do
                 echo ${key} - ${commands[${key}]}
