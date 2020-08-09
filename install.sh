146e51fbec4e143676e2c0fb618f2e8ae37618bb
ash <ash@max>
2020-08-09 22:31:13 +0530

    Installation Script Added

---
+++ install.sh
@@ -0,0 +1,12 @@
+if [ "$1" = "-uninstall" ] || [ "$1" = "-u" ]; then
+    rm -rf ~/.dyno
+    sed -i '/source ~\/.dyno\/dyno/d' ~/.bash_profile
+else
+    mkdir -p ~/.dyno
+    cp dyno ~/.dyno
+    cp template ~/.dyno
+    touch ~/.dyno/.nestedScripts
+    echo "source ~/.dyno/dyno" >> ~/.bash_profile
+    echo "!!!!!!!!!!!!!!!!!!!!!! Installation Complete !!!!!!!!!!!!!!!!!!!!!!!"
+    echo "             Restart Terminal for DYNO take effect  "
+fi
