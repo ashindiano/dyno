c8437065309c399038d8d8a0d2da14f26986b92b
ashindiano <ashindiano@gmail.com>
2022-04-03 10:49:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,14 @@ else
     cp dyno ~/.dyno
     cp template ~/.dyno
     touch ~/.dyno/.nestedScripts
+    echo "autoload -U compinit && compinit" >> ~/.bash_profile
+    echo "autoload -U bashcompinit && bashcompinit" >> ~/.bash_profile
     echo "source ~/.dyno/dyno" >> ~/.bash_profile
+    
+    echo "autoload -U compinit && compinit" >> ~/.zprofile
+    echo "autoload -U bashcompinit && bashcompinit" >> ~/.zprofile
+    echo "source ~/.dyno/dyno" >> ~/.zprofile
+    
     echo "!!!!!!!!!!!!!!!!!!!!!! Installation Complete !!!!!!!!!!!!!!!!!!!!!!!"
     echo "             Restart Terminal for DYNO take effect  "
 fi
